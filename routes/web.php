42f2434c84eb799dae3b697981562eed0114ec11
rmit-s3489536-kelvin-schlueter <rmit-s3489536-kelvin-schlueter@users.noreply.github.com>
2017-08-16 05:13:07 +1000

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,13 +9,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-
-
+Route::get('/home', 'HomeController@contact')->name('contact');
+Route::get('/home', 'HomeController@vehicle')->name('vehicle');
+Route::get('/home', 'HomeController@about')->name('about');
