a279a32a2e286b58998b58812c9f1899712cffbf
rmit-s3489536-kelvin-schlueter <rmit-s3489536-kelvin-schlueter@users.noreply.github.com>
2017-08-16 16:01:48 +1000

    Added navigation bar and some styles --

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,6 +84,30 @@
                  height: 400px;
                  width: 100%;
              }
+             
+             ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: #333;
+            }
+
+            li {
+                float: left;
+            }
+
+            li a {
+                display: block;
+                color: white;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+            }
+
+            li a:hover {
+                background-color: #111;
+            }
 
         </style>
     </head>
@@ -187,6 +211,16 @@
             <p><a class="btn btn-primary btn-lg" href="#" role="button">Book a Duck now!</a></p>
 
         </div>
+        
+        <div>
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">Vehicles</a></li>
+              <li><a href="">Contact</a></li>
+              <li><a href="">About</a></li>
+            </ul>
+        </div>
+        
         <div class="container" id='map'></div>
         <script>
             function initMap() {
