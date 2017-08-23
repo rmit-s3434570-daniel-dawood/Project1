f99f6ea24b69cb3d7a7681c00e121079bcee9258
rmit-s3489536-kelvin-schlueter <rmit-s3489536-kelvin-schlueter@users.noreply.github.com>
2017-08-23 16:16:06 +1000

    Update vehicles.blade.php

--- resources/views/vehicles.blade.php
+++ resources/views/vehicles.blade.php
@@ -1,9 +1,215 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
-@section('content')
-//list of vehicles offered (8-10)
-//-- add pages for vehicles (8-10) showing information about vehicles
-//allow for filter of vehicles
-//show if vehicles are available(?? or on individual vehicle page) 
-//show if booking available(?? or on individual vehicle page) 
-@endsection
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Contact Ducky</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+
+             ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: #333;
+            }
+
+            li {
+                float: left;
+            }
+
+            li a {
+                display: block;
+                color: white;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+            }
+
+            li a:hover {
+                background-color: #111;
+            }
+        
+        </style>
+    </head>
+
+{{--@extends('layouts/app')--}}
+
+    <div>
+        @if (Route::has('login'))
+            <div class="links" style="text-align:right">
+                @if (Auth::check())
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ url('/login') }}">Login</a>
+                    <a href="{{ url('/register') }}">Register</a>
+                @endif
+            </div>
+        @endif
+    </div>
+
+    <body>
+
+        <div class="jumbotron" style="text-align: center; background-color: #f9ea43" >
+
+            <h1>Ducky</h1>
+        </div>
+        
+        <div>
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">Vehicles</a></li>
+              <li><a href="">Locations</a></li>
+              <li><a href="">Contact</a></li>
+              <li><a href="">About</a></li>
+            </ul>
+        </div>
+        
+        <!-- Vehicles -->
+        <div class = "Vehicle-container" >
+        	<div class="Car-1" style="width:80%; height: 110px; align: center;">
+			    <div class="col-xs-12 col-sm-6 col-md-8" style="float:left">
+			        <h2> Car 1 </h2>
+			        <p>add image</p>
+			    </div>
+		    	<div class="col-xs-6 col-md-4" style="float:right">
+		    	    <h3>Pricing - $40 p/day</h3>
+		    	    <div class="panel panel-default">
+                      <div class="panel-body">
+                       Availability
+                      </div>
+                    </div>
+		    	    <p><a class="btn btn-primary btn-lg" href="{{ url('/booking/create') }}" role="button">Book now!</a></p>
+		    	</div>
+			</div>
+			<br>
+			<div class="Car-2" style="width:80%; height: 110px; align: center;">
+			    <div class="col-xs-12 col-sm-6 col-md-8" style="float:left" >
+			        <h2> Car 2 </h2>
+			        <p>add image</p>
+			    </div>
+		    	<div class="col-xs-6 col-md-4" style="float:right">
+		    	    <h3>Pricing - $30 p/day</h3>
+		    	    <div class="panel panel-default">
+                      <div class="panel-body">
+                       Availability
+                      </div>
+                    </div>
+		    	    <p><a class="btn btn-primary btn-lg" href="#" role="button">Book now!</a></p>
+		    	</div>
+			</div>
+			<br>
+			<div class="Car-3" style="width:80%; height: 110px; align: center;">
+			    <div class="col-xs-12 col-sm-6 col-md-8" style="float:left" >
+			        <h2> Car 3 </h2>
+			        <p>add image</p>
+			    </div>
+		    	<div class="col-xs-6 col-md-4" style="float:right">
+		    	    <h3>Pricing - $10 p/day</h3>
+		    	    <div class="panel panel-default">
+                      <div class="panel-body">
+                       Availability
+                      </div>
+                    </div>
+		    	    <p><a class="btn btn-primary btn-lg" href="#" role="button">Book now!</a></p>
+		    	</div>
+			</div>
+			<br>
+			<div class="Car-4" style="width:80%; height: 110px; align: center;">
+			    <div class="col-xs-12 col-sm-6 col-md-8" style="float:left" >
+			        <h2> Car 4</h2>
+			        <p>add image</p>
+			    </div>
+		    	<div class="col-xs-6 col-md-4" style="float:right">
+		    	    <h3>Pricing - $5 p/day</h3>
+		    	    <div class="panel panel-default">
+                      <div class="panel-body">
+                       Availability
+                      </div>
+                    </div>
+		    	    <p><a class="btn btn-primary btn-lg" href="#" role="button">Book now!</a></p>
+		    	</div>
+			</div>
+			
+			
+			
+			
+			
+			
+			
+			
+			
+        </div>
+        
+        
+        <br>
+        <br>
+        <br>
+        <!-- Footer -->
+        <div>
+            <ul>
+              <li><a href="">Footer Stuff here</a></li>
+            </ul> 
+        </div>
+        
+    </body>
+</html>
