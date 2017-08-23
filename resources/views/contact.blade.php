fdec029e867a7ec86ab14351fadb3a42e61f5509
rmit-s3489536-kelvin-schlueter <rmit-s3489536-kelvin-schlueter@users.noreply.github.com>
2017-08-23 16:15:30 +1000

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,9 +1,162 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
-@section('content')
-//Add form for customer details
-//Add input box for complaint/suggestion
-//Add section showing location of company (RMIT)
-//Add map(?)showing location of company
-//Edit page 
-@endsection
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Contact Ducky</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+
+             ul {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: #333;
+            }
+
+            li {
+                float: left;
+            }
+
+            li a {
+                display: block;
+                color: white;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+            }
+
+            li a:hover {
+                background-color: #111;
+            }
+        
+        </style>
+    </head>
+
+{{--@extends('layouts/app')--}}
+
+    <div>
+        @if (Route::has('login'))
+            <div class="links" style="text-align:right">
+                @if (Auth::check())
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ url('/login') }}">Login</a>
+                    <a href="{{ url('/register') }}">Register</a>
+                @endif
+            </div>
+        @endif
+    </div>
+
+    <body>
+
+        <div class="jumbotron" style="text-align: center; background-color: #f9ea43" >
+
+            <h1>Ducky</h1>
+        </div>
+        
+        <div>
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">Vehicles</a></li>
+              <li><a href="">Locations</a></li>
+              <li><a href="">Contact</a></li>
+              <li><a href="">About</a></li>
+            </ul>
+        </div>
+        
+        <!-- Html Form / Bootstrap -->
+        <div>
+            <form class="form-horizontal">
+              <fieldset>
+                <legend>Contact Ducky</legend>
+                <div class="form-group">
+                    <label for="inputEmail" class="col-lg-2 control-label">Email</label>
+                    <div class="col-lg-10">
+                        <input type="text" class="form-control" id="inputEmail" placeholder="Email">
+                    </div>
+                </div>
+                <div class="form-group">
+                  <label for="textArea" class="col-lg-2 control-label">Comment Here</label>
+                  <div class="col-lg-10">
+                    <textarea class="form-control" rows="3" id="textArea"></textarea>
+                  </div>
+                </div>
+                <br>
+                <div class="form-group">
+                  <div class="col-lg-10 col-lg-offset-2">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                  </div>
+                </div>
+              </fieldset>
+            </form>
+        </div>
+        
+        <!-- Footer -->
+        <div>
+            <ul>
+              <li><a href="">Footer Stuff here</a></li>
+            </ul> 
+        </div>
+        
+    </body>
+</html>
