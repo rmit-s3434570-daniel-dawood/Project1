d8942f1231fe5f2f0cc5c5028c8d89616d052a2a
rmit-s3489536-kelvin-schlueter <rmit-s3489536-kelvin-schlueter@users.noreply.github.com>
2017-10-04 16:18:03 +1100

    Add files via upload

--- resources/views/vehicles.blade.php
+++ resources/views/vehicles.blade.php
@@ -130,6 +130,18 @@
               <li><a href="/locations">Locations</a></li>
               <li><a href="/contact">Contact</a></li>
               <li><a href="/about">About</a></li>
+
+              <div class = "pull-right">
+                <form class="navbar-form">
+		            <div class="input-group">
+			            <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+			            <div class="input-group-btn">
+				            <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+			            </div>
+		            </div>
+		        </form> 
+              </div>
+              
             </ul>
         </div>
         
