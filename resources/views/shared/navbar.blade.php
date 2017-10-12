9ff3eb8995be1eac97bb33acb2c036d5f52ee45c
rmit-shaunsuares <s3383033@student.rmit.edu.au>
2017-10-13 07:08:26 +1100

    Add files via upload

--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -1,10 +1,11 @@
 <div>
-    <ul>
+    <ul class="topnav">
         <li><a href="/">Home</a></li>
         <li><a href="vehicles">Vehicles</a></li>
         <li><a href="locations">Locations</a></li>
         <li><a href="contact">Contact</a></li>
         <li><a href="about">About</a></li>
+
         
         <div class = "pull-right">
             <form class="navbar-form">
@@ -18,4 +19,5 @@
         </div>
         
     </ul>
+    <br><br>
  </div>
